d8dec75bc8c967ab802fcdfd1c4d37e69a4e12a1
Lucas Abreu <lucas.dfa@live.com>
2022-12-11 21:02:39 -0300

    criacao da service para petcarer, criacao do contexto e alteracao da controller

---
+++ Services/PetCarerService/PetCarerService.cs
@@ -0,0 +1,62 @@
+﻿using GeoPet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeoPet.Services.PetCarerService
+{
+    public class PetCarerService : IPetCarerService
+    {
+        private readonly GeoPetContext _context;
+
+        public PetCarerService(GeoPetContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PetCarer>> AddPetCarer(PetCarer body)
+        {
+            _context.PetCarers.Add(body);
+            await _context.SaveChangesAsync();
+            return await _context.PetCarers.ToListAsync();
+        }
+
+        public async Task<List<PetCarer>?> DeletePetCarer(int id)
+        {
+            var petCarer = await _context.PetCarers.FindAsync(id);
+
+            if (petCarer is null) return null;
+
+            _context.PetCarers.Remove(petCarer);
+            await _context.SaveChangesAsync();
+            return await _context.PetCarers.ToListAsync();
+        }
+
+        public async Task<List<PetCarer>> GetAllPetCarers()
+        {
+            var result = await _context.PetCarers.ToListAsync();
+            return result;
+        }
+
+        public async Task<PetCarer?> GetPetCarersById(int id)
+        {
+            var petCarer = await _context.PetCarers.FindAsync(id);
+            if (petCarer is null) return null;
+            return petCarer;
+        }
+
+        public async Task<List<PetCarer>?> UpdatePetCarer(int id, PetCarer body)
+        {
+            var petCarer = await _context.PetCarers.FindAsync(id);
+
+            if (petCarer is null) return null;
+
+            petCarer.Name = body.Name;
+            petCarer.Email = body.Email;
+            petCarer.ZipCode = body.ZipCode;
+            petCarer.Password = body.Password;
+
+            await _context.SaveChangesAsync();
+
+            return await _context.PetCarers.ToListAsync();
+        }
+    }
+}
