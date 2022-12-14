83359e59b6a1a05d2991338d16f2d10818b84227
Lucas Abreu <lucas.dfa@live.com>
2022-12-14 15:11:32 -0300

    subindo

---
+++ Controllers/GenerateQrCodeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IronBarCode;
+using System;
+using System.Drawing;
+using System.Linq;
+using GeoPet.Services.PetService;
+
+namespace GeoPet.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GenerateQrCodeController : ControllerBase
+    {
+        private readonly IPetService _petService;
+
+        public GenerateQrCodeController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet]
+        [Route("{id")]
+        public async Task<ActionResult<GeneratedBarcode>> GetQrCode(int petId)
+        {
+            var pet = await _petService.GetPetsById(petId);
+            if (pet == null) return NotFound();
+            var convertPet = pet.ToString();
+
+            GeneratedBarcode barcode = QRCodeWriter.CreateQrCode(convertPet, 500, QRCodeWriter.QrErrorCorrectionLevel.Medium).SaveAsHtmlFile("PetInformation.html");
+            return barcode;
+        }
+    }
+}
--- Controllers/searchController.cs
+++ Controllers/searchController.cs
@@ -1,23 +1,17 @@
-﻿using GeoPet.Models.Responses;
-using GeoPet.Services.SearchService;
+﻿using GeoPet.Services.SearchService;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.SqlServer.Server;
-using Newtonsoft;
-using Newtonsoft.Json;
-using System.Globalization;
-using System.Net.Http;
 
 namespace GeoPet.Controllers
 {
     [Route("[Controller]")]
     [ApiController]
-    public class searchController : ControllerBase
+    public class SearchController : ControllerBase
     {
         private readonly ISearchService _searchService;
 
-        public searchController(ISearchService searchService)
+        public SearchController(ISearchService searchService)
         {
-            searchService = _searchService;
+            _searchService = searchService;
         }
         [HttpGet]
         public async Task<IActionResult> GetAddress(double lat, double lon)
